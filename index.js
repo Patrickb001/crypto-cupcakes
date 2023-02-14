a75d8c5069807446c221244f8426a1890543edc6
Patrick Borgella <patrickborgella1@gmail.com>
2023-02-14 11:37:49 -0600

    Creates /me route after user logs in to return token, and user object, adds user to db

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const { PORT = 3000 } = process.env;
+const jwt = require("jsonwebtoken");
 // TODO - require express-openid-connect and destructure auth from it
 const { auth } = require("express-openid-connect");
 
@@ -20,8 +21,13 @@ app.use(express.urlencoded({ extended: true }));
 // follow the docs:
 
 // define the config object
-const { AUTH0_issuerBaseURL, AUTH0_clientID, AUTH0_baseURL, AUTH0_SECRET } =
-  process.env;
+const {
+  AUTH0_issuerBaseURL,
+  AUTH0_clientID,
+  AUTH0_baseURL,
+  AUTH0_SECRET,
+  JWT_SECRET,
+} = process.env;
 
 const config = {
   authRequired: true,
@@ -34,10 +40,33 @@ const config = {
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
+app.use(async (req, res, next) => {
+  const { name, email } = req.oidc.user;
+  const [user] = await User.findOrCreate({
+    where: {
+      username: req.oidc.user.nickname,
+      name,
+      email,
+    },
+  });
+  console.log(user);
+  next();
+});
 
+app.use(express.static("images"));
 // req.isAuthenticated is provided from the auth router
 app.get("/", (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+  // console.log(req.oidc.user);
+
+  res.send(`
+  <div>
+  <h1 style="text-align: center;"> My Web App Inc</h1>
+  <h2>Welcome, ${req.oidc.user.name}</h2>
+  <h3>Username ${req.oidc.user.nickname}</h3>
+  <p>email: ${req.oidc.user.email}</p>
+  <img src=${req.oidc.user.picture} alt="User profile picture"  />
+  </div>
+  `);
 });
 
 app.get("/cupcakes", async (req, res, next) => {
@@ -46,6 +75,22 @@ app.get("/cupcakes", async (req, res, next) => {
     res.send(cupcakes);
   } catch (error) {
     console.error(error);
+    next(error);
+  }
+});
+
+app.get("/me", async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: { username: req.oidc.user.nickname },
+      raw: true,
+    });
+    if (user) {
+      const token = jwt.sign(user, JWT_SECRET, { expiresIn: "1w" });
+      res.send({ user, token });
+    }
+  } catch (error) {
+    console.log(error);
     next(error);
   }
 });
